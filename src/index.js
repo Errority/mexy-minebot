6eaa9321359e1d420e50fda710a2e7fcb7712657
Mexywastaken <76711509+mexywastaken@users.noreply.github.com>
2021-10-26 17:52:44 +0200

    Skeleton

--- src/index.js
+++ src/index.js
@@ -5,28 +5,29 @@ const mineflayer = require('mineflayer');
 const pathfinder = require('mineflayer-pathfinder').pathfinder;
 const collect = require('mineflayer-collectblock').plugin;
 
+/**
+ * 
+ * @param {mineflayer.Bot} bot 
+ */
+function loadPlugins(bot) {
+	const dir = path.join(__dirname, 'plugins');
+	const modules = fs
+		.readdirSync(dir)
+		.filter(f => f.endsWith('.js'))
+		.map(name => require(path.join(dir, name)));
 
 
-  function loadPlugins(bot) {
-    const dir = path.join(__dirname, 'plugins');
-     const modules = fs 
-     .readdirSync(dir)
-     .filter(f => f.endsWith('.js'))
-     .map(name => require(path.join(dir, name)));
+	bot.loadPlugins(modules)
+	bot.loadPlugin(pathfinder);
+	bot.loadPlugin(collect);
+
+};
 
 
-    bot.loadPlugins(modules)
-    bot.loadPlugin(pathfinder);
-    bot.loadPlugin(collect);
+function init() {
+	const bot = mineflayer.createBot(login);
+	loadPlugins(bot)
+};
 
-
-  };
-
-
-  function init() {
-  const bot = mineflayer.createBot(login);
-  loadPlugins(bot)
-  };
-
-init();
+init()
 
